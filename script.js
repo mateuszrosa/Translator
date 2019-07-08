d7165f8eece2bab067ce1d1ee5170cbb3b3a68fa
mateuszrosa <mateusz.rosa@gmail.com>
2019-07-08 11:42:00 +0200

    form html and js handle

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('work');
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log(input.value);
+    input.value = '';
+})

67b57f3d72be3a950afe01d58399b836842216d2
mateuszrosa <mateusz.rosa@gmail.com>
2019-07-09 22:17:36 +0200

    handle choose fromLang

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const second = document.querySelector('.second');
 const input = document.querySelector('.input');
 const spans = document.querySelectorAll('.choose span');
 let text = '';
+let fromLang = 'en';
+let toLang = 'pl'
 const key = `trnsl.1.1.20190707T201153Z.e127b502ca8c8497.8d4de021cacefbe69e6f3ecf754746c2f092c15d`;
 
 input.addEventListener('input', e => {
@@ -14,11 +16,12 @@ spans.forEach(span => {
     span.addEventListener('click', e => {
         spans.forEach(span => span.classList.remove('active'));
         e.target.classList.toggle('active');
+        fromLang = e.target.dataset.lang;
     })
 })
 
 translate = () => {
-    fetch(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${text}&lang=en-pl&[format=plain]`)
+    fetch(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${text}&lang=${fromLang}-${toLang}&[format=plain]`)
         .then(response => {
             return response;
         })

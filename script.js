ebc708522746ddded32303f5fa9305cad9f4a272
mateuszrosa <mateusz.rosa@gmail.com>
2019-07-09 20:07:36 +0200

    spans styled

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const first = document.querySelector('.first');
 const second = document.querySelector('.second');
 const input = document.querySelector('.input');
+const spans = document.querySelectorAll('.choose span');
 let text = '';
 const key = `trnsl.1.1.20190707T201153Z.e127b502ca8c8497.8d4de021cacefbe69e6f3ecf754746c2f092c15d`;
 
-input.addEventListener('click', e => {
-    e.target.textContent = '';
-})
 input.addEventListener('input', e => {
-    text = e.target.textContent;
+    text = e.target.value;
     translate();
+});
+
+spans.forEach(span => {
+    span.addEventListener('click', () => {
+        console.log('span');
+    })
 })
 
 translate = () => {

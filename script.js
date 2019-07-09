872d000de75bb49a2cb7176f777569a31797809b
mateuszrosa <mateusz.rosa@gmail.com>
2019-07-09 18:01:39 +0200

    removing span after click on div.input

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 const first = document.querySelector('.first');
 const second = document.querySelector('.second');
-const input = document.querySelector('input');
+const input = document.querySelector('.input');
 let text = '';
 const key = `trnsl.1.1.20190707T201153Z.e127b502ca8c8497.8d4de021cacefbe69e6f3ecf754746c2f092c15d`;
 
 
-first.addEventListener('click', e => {
-    e.target.textContent = '';
+input.addEventListener('click', e => {
+    input.querySelector('span').textContent = '';
 })
 
-
-first.addEventListener('input', e => {
-    text = e.target.textContent;
-    translate();
-})
 
 translate = () => {
     fetch(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${text}&lang=en-pl&[format=plain]`)

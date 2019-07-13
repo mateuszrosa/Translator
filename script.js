a37d6c56fe4f25c0c3b3e4e5e3d4b5a8a020b359
mateuszrosa <mateusz.rosa@gmail.com>
2019-07-13 13:46:51 +0200

    deleted detect span and detect function

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const translator = document.querySelector('.second .translator');
 const input = document.querySelector('.input');
-const spansFrom = document.querySelectorAll('.first .choose li');
-const spansTo = document.querySelectorAll('.second .choose li');
-const detectSpan = document.querySelector('li.detect');
+const lisFrom = document.querySelectorAll('.first .choose li');
+const lisTo = document.querySelectorAll('.second .choose li');
 const btn = document.querySelector('button');
 let translation = '';
 let fromLang = 'pl';
-let toLang = 'en'
+let toLang = 'en';
 const key = `trnsl.1.1.20190707T201153Z.e127b502ca8c8497.8d4de021cacefbe69e6f3ecf754746c2f092c15d`;
 
 input.addEventListener('click', () => {
@@ -24,25 +23,17 @@ btn.addEventListener('click', () => {
     }
 })
 
-spansFrom.forEach(span => {
-    span.addEventListener('click', e => {
-        spansFrom.forEach(span => span.classList.remove('active'));
+lisFrom.forEach(li => {
+    li.addEventListener('click', e => {
+        lisFrom.forEach(span => span.classList.remove('active'));
         e.target.classList.toggle('active');
-        if (e.target.dataset.lang === "detect") {
-            input.addEventListener('input', e => {
-                if (input.value.length > 2) {
-                    detect(input.value);
-                }
-            })
-        } else {
-            fromLang = e.target.dataset.lang;
-        }
+        fromLang = e.target.dataset.lang;
     })
 })
 
-spansTo.forEach(span => {
-    span.addEventListener('click', e => {
-        spansTo.forEach(span => span.classList.remove('active'));
+lisTo.forEach(li => {
+    li.addEventListener('click', e => {
+        lisTo.forEach(li => li.classList.remove('active'));
         e.target.classList.toggle('active');
         toLang = e.target.dataset.lang;
     })
@@ -60,18 +51,5 @@ const translate = (text) => {
         .then(data => {
             console.log(data);
             translator.textContent = data.text[0];
-        })
-}
-
-const detect = (text) => {
-    fetch(`https://translate.yandex.net/api/v1.5/tr.json/detect?key=${key}&text=${text}`)
-        .then(response => {
-            if (response.ok) return response
-        })
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            detectSpan.textContent = `Detect: ${data.lang}`
         })
 }

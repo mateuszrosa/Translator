94a63ae06924f48a5923c11aceb1721d23312900
mateuszrosa <mateusz.rosa@gmail.com>
2019-07-23 18:15:17 +0200

    print length of keys inside data.langs object

--- script.js
+++ script.js
@@ -90,6 +90,7 @@ fetch(`https://translate.yandex.net/api/v1.5/tr.json/getLangs?ui=en&key=${key}`)
     })
 
 const fillMore = (data) => {
+    console.log(Object.keys(data).length);
     for (let [key, value] of Object.entries(data)) {
         newLi = document.createElement('li');
         newLi.textContent = value;
